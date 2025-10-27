065ec360dd1b224c3c0b9e0b7b6d97d47b6e9536
Tafsirchy <tafsirchy1000@gmail.com>
2025-10-28 00:47:19 +0600

    Navigate useLocation and Module Summary

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,22 +1,33 @@
 import React, { Suspense, useState } from "react";
-import { Link } from "react-router";
+import { Link, Navigate, useLocation } from "react-router";
 import UserDetails2 from "../UserDetails2/UserDetails2";
 
 const User = ({ user }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  const [visitHome, setVisitHome] = useState(false);
+  const location = useLocation();
+  console.log(location);
+
   const { id, name, email, phone } = user;
 
-  const userPromise = fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-    .then((res) => res.json());
 
-  const [showInfo, setShowInfo] = useState(false)
+
+  const userPromise = fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  ).then((res) => res.json());
+
+
 
   const userStyles = {
     border: "2px solid olive",
-    borderRadius: '20px',
-    padding: '10px',
-    margin: '20px',
-
+    borderRadius: "20px",
+    padding: "10px",
+    margin: "20px",
   };
+
+  if (visitHome) {
+    return <Navigate to={"/"}></Navigate>
+  }
 
   return (
     <div style={userStyles}>
@@ -29,11 +40,12 @@ const User = ({ user }) => {
       <button onClick={() => setShowInfo(!showInfo)}>
         {showInfo ? "Hide" : "Show"} Info
       </button>
-      {showInfo && (
+      {showInfo && 
         <Suspense fallback={<p>Loading...</p>}>
           <UserDetails2 userPromise={userPromise}></UserDetails2>
         </Suspense>
-      )}
+      }
+      <button onClick={() => setVisitHome(true)} >Visit Home</button>
     </div>
   );
 };
